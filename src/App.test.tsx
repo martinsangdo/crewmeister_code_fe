dd63739c45ea7c5efed7cc0a4e667e39131c4d64
sangdo <sang.do@knorex.com>
2022-12-08 06:22:37 +0700

    - add test rendering

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Absences from './components/absences';
+
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
 
-test('renders absences link', () => {
+//some required components must be in the page
+test('renders absences components', () => {
   const mockStore = configureStore();
   let store = mockStore({});
   render(<Provider store={store}><Absences/></Provider>);
-  const linkElement = screen.getByText(/Search absences by date/i);
-  expect(linkElement).toBeInTheDocument();
+  const btnSearch = screen.getByText(/Search absences by date/i); //this is button
+  const total = screen.getByText(/Total items/i); //total items
+  //todo more
+  expect(btnSearch).toBeInTheDocument();
+  expect(total).toBeInTheDocument();
 });
+
