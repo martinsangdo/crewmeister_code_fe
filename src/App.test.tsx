f34a19d230b835f07f89e76113104d8de13ce4ed
sangdo <sang.do@knorex.com>
2022-12-07 17:00:51 +0700

    - init test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Absences from './components/absences';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders absences link', () => {
+  const mockStore = configureStore();
+  let store = mockStore({});
+  render(<Provider store={store}><Absences/></Provider>);
+  const linkElement = screen.getByText(/Search absences by date/i);
   expect(linkElement).toBeInTheDocument();
 });
